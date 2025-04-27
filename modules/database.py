b54a084de2b95bfea03e8b59847d4756354d8b1b
Alessandro <2ale2>
2025-04-27 12:03:12 +0200

    added little safety measure

--- modules/database.py
+++ modules/database.py
@@ -81,7 +81,7 @@ async def add_to_table(table_name: str, content: dict):
         if table_name == "main_table":
             query += (
                 f"ON CONFLICT (user_id) DO UPDATE SET "
-                f"points = CASE WHEN {table_name}.points + 1 >= {soglia} THEN 0 ELSE {table_name}.points + 1 END, "
+                f"points = CASE WHEN {table_name}.points + 1 >= {int(soglia)} THEN 0 ELSE {table_name}.points + 1 END, "
                 f"total = {table_name}.total + 1,"
                 f"username = EXCLUDED.username "
                 f"RETURNING points"
